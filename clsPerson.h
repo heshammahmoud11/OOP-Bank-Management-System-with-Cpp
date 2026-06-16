018b345169445d0f9cfecb8d079e541064d6c828
heshammahmoud11 <heshammahmoud646@gmail.com>
2026-06-16 13:38:54 +0300

    Modify : Modefy some functions names in person class

--- clsPerson.h
+++ clsPerson.h
@@ -1,86 +1,97 @@
 #pragma once
-
-#include<iostream> 
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-
-
 
 class clsPerson
 {
-private : 
-    string _firstName;
-    string _lastName;
-    string _email;
-    string _phone;
 
+private:
+   
+    string _FirstName;
+    string _LastName;
+    string _Email;
+    string _Phone;
 
-public :
-    clsPerson(string firstName, string lastName, string email, string phone)
+public:
+ 
+    clsPerson( string FirstName, string LastName, string Email, string Phone)
     {
-        _firstName = firstName;
-        _lastName = lastName;
-        _email = email;
-        _phone = phone;
+        
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Email = Email;
+        _Phone = Phone;
     }
 
-    // setters and getters (set = put, get = get)
-    
-    void setFirstName(string fName)
+    //Property Set
+    void SetFirstName(string FirstName)
     {
-        _firstName = fName;
+        _FirstName = FirstName;
+    }
+
+    //Property Get
+    string GetFirstName()
+    {
+        return _FirstName;
+    }
+    // __declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+
+    //Property Set
+    void SetLastName(string LastName)
+    {
+        _LastName = LastName;
+    }
+
+    //Property Get
+    string GetLastName()
+    {
+        return _LastName;
+    }
+    // __declspec(property(get = GetLastName, put = SetLastName)) string LastName;
+
+    //Property Set
+    void SetEmail(string Email)
+    {
+        _Email = Email;
+    }
+
+    //Property Get
+    string GetEmail()
+    {
+        return _Email;
+    }
+    // __declspec(property(get = GetEmail, put = SetEmail)) string Email;
+
+    //Property Set
+    void SetPhone(string Phone)
+    {
+        _Phone = Phone;
+    }
+
+    //Property Get
+    string GetPhone()
+    {
+        return _Phone;
+    }
+    // __declspec(property(get = GetPhone, put = SetPhone)) string Phone;
+
+    string FullName()
+    {
+        return _FirstName + " " + _LastName;
+    }
+
+    void Print()
+    {
+        cout << "\nInfo:";
+        cout << "\n___________________";
+        cout << "\nFirstName: " << _FirstName;
+        cout << "\nLastName : " << _LastName;
+        cout << "\nFull Name: " << FullName();
+        cout << "\nEmail    : " << _Email;
+        cout << "\nPhone    : " << _Phone;
+        cout << "\n___________________\n";
 
     }
 
-    string getFirstName()
-    {
-        return _firstName;
-    }
-
-    void setLastName(string lName)
-    {
-        _lastName = lName;
-    }
-
-    string getLastName()
-    {
-        return _lastName;
-    }
-
-    void setEmail(string email)
-    {
-        _email = email;
-    }
-
-    string getEmail()
-    {
-        return _email;
-    }
-
-    void setPhone(string phone)
-    {
-        _phone = phone;
-    }
-
-    string getPhone()
-    {
-        return _phone;
-    }
-
-
-    string getFullName()
-    {
-        return _firstName + " " + _lastName;
-    }
-
-    void print()
-    {
-        cout << "INFO : \n";
-        cout << "------------------------\n";
-        cout << "First Name : " << _firstName << "\n";
-        cout << "Last Name : " << _lastName << "\n";
-        cout << "Full Name : " << getFullName() << "\n";
-        cout << "Email : " << _email << "\n";
-        cout << "Phoe : " << _phone << "\n";
-    }
 };
