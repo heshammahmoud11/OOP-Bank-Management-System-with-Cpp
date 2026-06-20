5feb72ec3d60d9616dba691475f4d6d269bf6b56
heshammahmoud11 <heshammahmoud646@gmail.com>
2026-06-21 01:56:59 +0300

    Add 2 functions : 1- Add New Client, 2- Delete Client  to the main cpp file

--- main.cpp
+++ main.cpp
@@ -65,13 +65,92 @@ void UpdateClient()
     }
 
 
+}
+
+void AddNewClient()
+{
+    string AccountNumber = "";
+    cout << "\nPlease Enter Account Number: ";
+
+    AccountNumber = clsInputValidate::ReadString();
+
+    while(clsBankClient::IsClientExist(AccountNumber))
+    {
+        cout << "\nThe Client Number is Already Exist, Please Enter another account number : ";
+        AccountNumber = clsInputValidate::ReadString();
+    }
+
+    clsBankClient NewClient = clsBankClient::GetAddNewClientObject(AccountNumber);
+
+    ReadClientInfo(NewClient);
+
+    clsBankClient::enSaveResults SaveResult = NewClient.Save();
+
+
+    switch (SaveResult)
+    {
+        case clsBankClient::enSaveResults::svSucceeded :
+        {
+            cout << "\nClient Addedd Successfully -:)\n";
+            break;
+        }
+        case clsBankClient::enSaveResults::svFaildEmptyObject :
+        {
+            cout << "\nError, Account Was not Saved because it's Empty\n";
+            break; 
+        }
+        case clsBankClient::enSaveResults::svFaildAccountNumberExists :
+        {
+            cout << "\nError, Account Number is Already Exist.";
+            break;
+        }
+    }
+
 
 }
+
+void DeleteClient()
+{
+    string AccountNumber = "";
+    cout << "\nEnter Account Number to Delete: ";
+    AccountNumber = clsInputValidate::ReadString();
+
+    while(!clsBankClient::IsClientExist(AccountNumber))
+    {
+        cout << "\nAccount Number Not Found, Enter another One: ";
+        AccountNumber = clsInputValidate::ReadString();
+    }
+
+    clsBankClient client = clsBankClient::Find(AccountNumber);
+    client.Print();
+
+    cout << "\nAre You Sure Do You want to Delete This Client?[Y/N]: ";
+
+    char ans = 'n';
+    cin >> ans;
+
+    if(tolower(ans) == 'y')
+    {
+        if(client.Delete())
+        {
+            cout << "\nClient Deleted Successfully :-)\n";
+            client.Print();
+        }else
+        {
+            cout << "\nError, Client Was not Deleted\n";
+        }
+    }
+
+}
+  
+
 
 int main()
 {
-    UpdateClient();
-
+    
     return 0;
 }
+
+
+
 
